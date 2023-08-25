eaad3cf58fd02745ef0425c66a8ae427a1bf0bcb
sagadavid <epostsaga@gmail.com>
2023-08-26 00:34:33 +0200

    pass dynamic-static parameter into route path

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -7,10 +7,17 @@ import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PublicModule } from './public/public.module';
 
 @NgModule({
   declarations: [AppComponent, AboutUsComponent, HowItWorksComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, UserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    UserModule,
+    PublicModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ bookStoreApp/src/app/public/public-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllBooksComponent } from './components/all-books/all-books.component';
+import { PublicComponent } from './public.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'public',
+    component: PublicComponent,
+    children: [
+      { path: 'all-books', component: AllBooksComponent },
+      {
+        path: 'book-details/:id/author/:authorId',
+        component: BookDetailsComponent,
+      },
+      { path: 'home', component: HomeComponent },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PublicRoutingModule {}
