22de14475375d79c9ba3520760dddc8b080148f3
sagadavid <epostsaga@gmail.com>
2023-08-27 01:43:57 +0200

    NotFound routing component

--- bookStoreApp/src/app/app-routing.module.ts
+++ bookStoreApp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
   { path: 'how-it-works', component: HowItWorksComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
