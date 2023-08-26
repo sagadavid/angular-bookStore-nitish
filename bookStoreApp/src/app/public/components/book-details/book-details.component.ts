8272b6d56f5913acc28471a201b52f1be5fc7c46
sagadavid <epostsaga@gmail.com>
2023-08-26 09:29:56 +0200

    get parameters activatedRoutes1

--- bookStoreApp/src/app/public/components/book-details/book-details.component.ts
+++ bookStoreApp/src/app/public/components/book-details/book-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { __param } from 'tslib';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.css']
+  styleUrls: ['./book-details.component.css'],
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  public bookId: number = 0;
+  public authorId: number = 0;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    console.log(this.route);
+    //lets see what value parameters are obtained by activatedroute
+    this.route.params.subscribe((param) => {
+      console.log(param);
+    });
+  }
 }
